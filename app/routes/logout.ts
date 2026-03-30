8d45991f6cefd5e4c275dc7b40631efce35730f5
marximillion <marximillion@gmail.com>
2026-03-30 02:13:14 -0600

    Add loader to prevent unauthenticated access

--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -1,6 +1,10 @@
 import { redirect } from "react-router";
 import { sessionStorage } from "../db/session.server";
 
+export async function loader() {
+  return redirect("/");
+}
+
 export async function action({ request }: { request: Request }) {
   const session = await sessionStorage.getSession(
     request.headers.get("Cookie")
