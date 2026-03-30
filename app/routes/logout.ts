300eba617b439301f5103d86e9e4bfbbd38126bf
marximillion <marximillion@gmail.com>
2026-03-30 01:32:42 -0600

    Add logout

---
+++ app/routes/logout.ts
@@ -0,0 +1,11 @@
+import { redirect } from "react-router";
+import { sessionStorage } from "../db/session.server";
+
+export async function action({ request }: { request: Request }) {
+  const session = await sessionStorage.getSession(
+    request.headers.get("Cookie")
+  );
+  return redirect("/", {
+    headers: { "Set-Cookie": await sessionStorage.destroySession(session) },
+  });
+}
