550cc32c353faacfabd7b8c33f0fe2f2b712e05a
marximillion <marximillion@gmail.com>
2026-08-04 18:18:01 -0600

    [UI] Add transparent variant to Banner component

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,12 +1,13 @@
-// Copyright © MJDG 2026
+// Copyright © MJMDG 2026
 interface BannerProps {
   image: string;
   alt?: string;
+  transparent?: boolean;
 }
 
-export default function Banner({ image, alt = "Banner" }: BannerProps) {
+export default function Banner({ image, alt = "Banner", transparent = false }: BannerProps) {
   return (
-    <div className="bannerContainer">
+    <div className={transparent ? "bannerContainer bannerContainer--transparent" : "bannerContainer"}>
       <img src={image} alt={alt} style={{ height: "100%", objectFit: "contain" }} />
     </div>
   );
